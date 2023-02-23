7704bbd642c32de18ac39d47971e5e435de99e37
VitBuk <vitbuk@gmail.com>
2023-02-24 01:34:33 +0200

    person item manytoone relationship

--- src/main/java/com/vitbuk/spring/App.java
+++ src/main/java/com/vitbuk/spring/App.java
@@ -1,13 +1,27 @@
 package com.vitbuk.spring;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import java.lang.module.Configuration;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        Configuration configuration = new Configuration();
+        SessionFactory sessionFactory =  configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+
+            session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
     }
 }
